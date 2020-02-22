dca3edb0aea7f513670a0bbc69a83c23d73a1ffa
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-02-22 10:04:10 +0200

    1

---
+++ Modul2_Masivi/09_PP_Masiv_Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_PP_Masiv_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = new int[5];
+
+            int[] num = new int[] { 3, 5, 6, -7, 8 };
+        }
+    }
+}
