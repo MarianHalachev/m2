4a7a7859a7c35e041f6aebbfb90e53d40821fb25
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-02-22 11:04:01 +0200

    1

---
+++ Modul2_Masivi/10_PP_Zad2_Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_PP_Zad2_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            //Обръщане на елементи на масив
+            Array.Reverse(words);
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+}
---
+++ Modul2_Masivi/Masivi_Vhod_Na_Edin_Red/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masivi_Vhod_Na_Edin_Red
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+             string[] num = Console.ReadLine().Split(' ').ToArray();
+
+
+            Console.WriteLine("=================");
+            Console.WriteLine("=======FOR=======");
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[{i}]={num[i]} ");
+            }
+
+            /////////////////////////////////////////
+
+            Console.WriteLine("====================");
+            Console.WriteLine("=======FOREACH======");
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+
+            ///////////////////////////////////////////
+
+            Console.WriteLine("=======================");
+            Console.WriteLine("======string.Join======");
+            Console.WriteLine(string.Join(" ", num));
+            Console.WriteLine("=======================");
+        }
+    }
+}
