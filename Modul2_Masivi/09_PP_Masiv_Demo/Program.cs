d9bfccf254afede354d7c37d47c56c3092fd8a90
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-02-22 10:45:48 +0200

    1

--- Modul2_Masivi/09_PP_Masiv_Demo/Program.cs
+++ Modul2_Masivi/09_PP_Masiv_Demo/Program.cs
@@ -10,9 +10,47 @@ namespace _09_PP_Masiv_Demo
     {
         static void Main(string[] args)
         {
-            //int[] num = new int[5];
+            int[] num = new int[5];
 
-            int[] num = new int[] { 3, 5, 6, -7, 8 };
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"num[{i}]= ");
+                num[i] = int.Parse(Console.ReadLine());
+
+            }
+           
+            Console.WriteLine();
+            Console.WriteLine("=======FOR=======");
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"num[{i}]={num[i]} ");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("=======FOREACH======");
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("======string.Join======");
+            Console.WriteLine(string.Join(" ",num));
+
+
+            ///////////////////////////////////////////////
+            
+
+            //int[] num1 = new int[] { 3, 5, 6, -7, 8 };
+
+            ////Console.WriteLine(num[0]);
+            ////Console.WriteLine(num1[2]);
+
+            //int[] num2 = {1,2,3,4,5};
+            ////Console.WriteLine(num2[2]);
+
+            //string[] days = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+            //Console.WriteLine(days[2]);
         }
     }
 }
---
+++ Modul2_Masivi/09_PP_Masiv_zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_PP_Masiv_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi den ot sedmicata kato chislo: ");
+            int day = int.Parse(Console.ReadLine());
+
+            string[] days = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+
+            if (day>=1 && day <=7)
+            {
+                Console.WriteLine(days[day-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day");
+            }
+        }
+    }
+}
