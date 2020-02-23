d54b2062bc98b337b33c5b502793f5a0a9b67c69
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-02-23 11:14:35 +0200

    2

---
+++ Modul2_Masivi/Masivi_Sort_Reverse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masivi_Sort_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 5, 8, 234 };
+
+            int sum = num.Sum();
+
+            Console.WriteLine(sum);
+
+            //Sort
+            Array.Sort(num);
+            Console.WriteLine(string.Join(" ", num));
+
+            foreach (var item in num)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i]+" ");
+            }
+            Console.WriteLine();
+            //Reverse
+            Array.Reverse(num);
+            Console.Write(string.Join("|", num));
+        }
+    }
+}
---
+++ Modul2_Masivi/Mnogomeren_Masiv/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnogomeren_Masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Двумерен масив
+            int[,] num = new int[2,3];
+            int[,] num1 = { { 1, 2, 3 }, { 4, 5, 6 }, };
+           
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"num[{i}, {j}]=");
+                    num[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            //печат на масив
+            for (int i = 0; i < 2; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"num[{i}, {j}]={num[i,j]} ");
+                     sum=+ num[i,j];
+                    
+                }
+                Console.WriteLine();
+                double avg = sum / 3;
+                Console.WriteLine(avg);
+                
+            } 
+            
+        }
+    }
+}
