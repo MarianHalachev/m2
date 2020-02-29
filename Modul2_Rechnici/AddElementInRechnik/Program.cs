fca6e4ca9501344074da13c22ef4571b6f7b2f92
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-02-29 12:26:20 +0200

    2

---
+++ Modul2_Rechnici/AddElementInRechnik/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddElementInRechnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            Console.Write("Broi Elementi : ");
+            int brEl = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < brEl; i++)
+            {
+                Console.Write("Ime: ");
+                string name = Console.ReadLine();
+                Console.Write("Nomer: ");
+                string number = Console.ReadLine();
+
+
+                if (phonebook.ContainsKey(name))
+                {
+                    phonebook[name] = number;
+                }
+                else
+                {
+                    phonebook.Add(name, number);
+                }
+               
+            }
+
+            foreach (var item in phonebook)
+            {
+                Console.WriteLine($"ime: {item.Key} => nomer: {item.Value}");
+            }
+            Console.WriteLine("==============================");
+            phonebook.Remove("Asen");
+            Console.WriteLine(string.Join(" ", phonebook));
+
+            int count = phonebook.Count;
+            Console.WriteLine($"Broi: {count}");
+        }
+    }
+}
---
+++ Modul2_Rechnici/AddElementsInDictionary_Na_Edin_Red/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddElementsInDictionary_Na_Edin_Red
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> a = new Dictionary<string, int>();
+            var linia = Console.ReadLine().Split();
+
+            while (true)
+            {
+                string stoka = linia[0];
+                if (stoka=="stop")
+                {
+                    break; 
+                }
+
+                int price = int.Parse(linia[1]);
+
+                if (a.ContainsKey(stoka))
+                {
+                    a[stoka] = price + price;
+                }
+                else
+                {
+                    a.Add(stoka, price);
+                }
+
+                
+
+                linia = Console.ReadLine().Split();
+
+            }
+            foreach (var item in a)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value} lv ");
+            }
+
+        }
+    }
+}
