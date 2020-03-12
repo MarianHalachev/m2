3cc03562eda4c64a85635febcb3ea721571f8ddb
MarianHalachev <61077624+MarianHalachev@users.noreply.github.com>
2020-03-12 09:11:10 +0200

    1

---
+++ Papanov/Zad1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            SortedDictionary < double, double> number = new SortedDictionary<double, double>();
+            
+            foreach (var item in numbers)
+            {
+                if (number.ContainsKey(item))
+                {
+                   number[item]+=1;
+                }
+                else
+                {
+                    number[item] = 1;
+                }
+               
+
+            }
+            foreach (var item in number)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
